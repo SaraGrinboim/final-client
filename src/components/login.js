7685f0a34c27d6ae965e7a1ed72623521715510a
Ester-Tofik <325202349@mby.co.il>
2022-08-03 17:00:23 +0300

    login work

--- src/components/login.js
+++ src/components/login.js
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import {get} from '../api/user'
-export default function Login(){
-    const [userName,setUserName]=useState('');
-    useEffect (()=>{
-        debugger
-       get().then((users)=>
-        setUserName(users.data))
-    },[])
+import { useEffect, useState } from "react";
+import axios from 'axios';
+export default function User() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getAllUsers();
+    }, [])
+    const getAllUsers = async () => {
+        try {
+            debugger
+            const res = await axios.get(`http://localhost:3333/user`);
+            setUsers(res.data);
+        } catch (err) {
+            console.log(err)
+        }
+    };
     return (
-        <div>
-            <h1>hello to {userName}</h1>
-        </div>
+        <>
+            {/* <div>hi for all the users</div> */}
+            {users && users.map(user =>
+                <div key={user.password}>{user.userName}</div>
+            )}
+        </>
     )
 }
